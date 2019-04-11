7845ab589e252891d09d3de5ab047a77ada0f726
csibman27 <tmolnair@gmail.hu>
2019-04-11 20:59:56 +0100

    version up1

--- app/src/main/java/com/example/locaui/activities/AddActivity.kt
+++ app/src/main/java/com/example/locaui/activities/AddActivity.kt
@@ -3,11 +3,16 @@ package com.example.locaui.activities
 import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import android.widget.Button
 import com.example.locaui.R
 import com.example.locaui.main.MainApp
 import com.example.locaui.model.WebMarkModel
 import kotlinx.android.synthetic.main.activity_add.*
+import kotlinx.android.synthetic.main.activity_add.view.*
 import kotlinx.android.synthetic.main.cardview.*
 import org.jetbrains.anko.AnkoLogger
 import org.jetbrains.anko.info
@@ -33,19 +38,40 @@ class AddActivity : AppCompatActivity(), AnkoLogger {
         }
         val button1 = findViewById<Button>(R.id.btnSubmitAdd)
         button1.setOnClickListener {
-            webMark.webName = textView1.text.toString().trim()
+            webMark.webName = textView1.text.toString()
             if (webMark.webName.isNotEmpty()) {
                 app!!.webMarks.add(webMark.copy())
                 info("Add Button pressed: $addText1")
-                app!!.webMarks.forEach { info("Add button pressed: ${it}") }
+                app!!.webMarks.forEach { info("Add button pressed: ${it.webName}") }
                 setResult(AppCompatActivity.RESULT_OK)
                 finish()
             }
 
 
         }
-        //class WebMarkAdapter constructor(private var webMarks: List<WebMarkModel>): RecyclerView.Adapter<WebMarkAdapter.MainHolder>() {
+        class WebMarkAdapter constructor(private var webMarks: List<WebMarkModel>) :
+            RecyclerView.Adapter<WebMarkAdapter.MainHolder>() {
 
+
+            override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MainHolder {
+                return MainHolder(LayoutInflater.from(parent.context).inflate(R.layout.cardview, parent, false))
+            }
+
+            override fun onBindViewHolder(holder: MainHolder, position: Int) {
+                val webmark = webMarks[holder.adapterPosition]
+                holder.bind(webmark)
+            }
+
+            override fun getItemCount(): Int = webMarks.size
+
+            inner class MainHolder constructor(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+                fun bind(webmark: WebMarkModel) {
+                   itemView.addText1.text= webMark.webName
+                    itemView.editText2.text = webMark.webUrl
+                }
+            }
         }
     }
+}
 
