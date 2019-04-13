9154eac4c956f026b12e04e5f31dbd3a5dde0436
csibman27 <tmolnair@gmail.hu>
2019-04-13 10:43:51 +0100

    version up1

--- app/src/main/java/com/example/locaui/activities/AddActivity.kt
+++ app/src/main/java/com/example/locaui/activities/AddActivity.kt
@@ -3,16 +3,19 @@ package com.example.locaui.activities
 import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.LinearLayout
 import com.example.locaui.R
 import com.example.locaui.main.MainApp
 import com.example.locaui.model.WebMarkModel
 import kotlinx.android.synthetic.main.activity_add.*
 import kotlinx.android.synthetic.main.activity_add.view.*
+import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.cardview.*
 import org.jetbrains.anko.AnkoLogger
 import org.jetbrains.anko.info
@@ -23,12 +26,25 @@ class AddActivity : AppCompatActivity(), AnkoLogger {
 
     var webMark = WebMarkModel()
     var app: MainApp? = null
-    val webMarks = ArrayList<WebMarkModel>();
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add)
         app = application as MainApp
+
+        val view = findViewById<RecyclerView>(R.id.myTxt)
+        view.layoutManager = LinearLayoutManager(this,LinearLayout.VERTICAL, false)
+
+        val myAdapter = WebMarkAdapter(WebMarkModel)
+        val webMarks = ArrayList<WebMarkModel>()
+        webMarks.add(WebMarkModel(webName = "lala",webUrl = "www.sg.hu"))
+
+
+//        pass the values to RvAdapter
+        val rvAdapter = WebMarkAdapter(webMarks)
+//        set the recyclerView to the adapter
+        view.adapter = rvAdapter
 
 
         val button2 = findViewById<Button>(R.id.btnBackAdd)
@@ -67,8 +83,8 @@ class AddActivity : AppCompatActivity(), AnkoLogger {
             inner class MainHolder constructor(itemView: View) : RecyclerView.ViewHolder(itemView) {
 
                 fun bind(webmark: WebMarkModel) {
-                   itemView.addText1.text= webMark.webName
-                    itemView.editText2.text = webMark.webUrl
+                    webMark.webName = itemView.addText1.toString()
+                    webMark.webUrl = itemView.editText2.toString()
                 }
             }
         }
